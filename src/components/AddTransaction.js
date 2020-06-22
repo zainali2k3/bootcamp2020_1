3b30fece0fa37e42cb2783a614a6de4783e0975d
Zain Ali <zainali_2k3@hotmail.com>
2020-06-22 23:47:22 +0500

    add transaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -28,7 +28,7 @@ export const AddTransaction = () => {
             type="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder="Enter text..."
+            placeholder="Enter text... "
           />
         </div>
         <div className="form-control">
